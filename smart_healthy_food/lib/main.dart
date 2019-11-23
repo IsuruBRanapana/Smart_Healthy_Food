57c686716a65affe3bf0b58b6f85b86ea1036039
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-11-23 12:40:45 +0530

    Updated

---
+++ smart_healthy_food/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:smart_healthy_food/app_screens/login.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
