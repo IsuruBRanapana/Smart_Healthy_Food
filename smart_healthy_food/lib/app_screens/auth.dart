caeddf86485e980ab2bfcf08a2a5c7a93d74f6e9
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-11-25 21:03:11 +0530

    Login Page imported

---
+++ smart_healthy_food/lib/app_screens/auth.dart
@@ -0,0 +1,61 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+abstract class BaseAuth{
+  Future<String> signInWithEmailAndPassword(String email, String password);
+  Future<void> signOut();
+  Future<bool> signInWithGoogle();
+  Future <String> currentUser();
+}
+
+class AuthService implements BaseAuth{
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  
+
+  //sign in with email
+  Future<String> signInWithEmailAndPassword(String email, String password) async {
+    AuthResult result = await FirebaseAuth.instance
+        .signInWithEmailAndPassword(email: email, password: password);
+    FirebaseUser user = result.user;
+    return user.uid;
+  }
+
+  //sign in with google
+  Future<String> signInWithCredential(){
+    //Todo: implement this method
+  }
+  Future<bool> signInWithGoogle() async{
+    try{
+      GoogleSignIn googleSignIn=GoogleSignIn();
+      GoogleSignInAccount account=await googleSignIn.signIn();
+      if(account==null){
+        return false;
+      }else{
+        AuthResult res=await _auth.signInWithCredential(GoogleAuthProvider.getCredential(
+          idToken: (await account.authentication).idToken,
+          accessToken: (await account.authentication).accessToken
+        )
+        );
+        if(res.user==null){
+          return false;
+        }else{
+          return true;
+        }
+      }
+    }catch (e){
+      print('error logging with google');
+      return false;
+    }
+  }
+
+  Future <String> currentUser() async{
+    FirebaseUser user =await FirebaseAuth.instance.currentUser();
+    return user.uid;
+  }
+  
+  //sign out
+  Future<void> signOut() async{
+    return FirebaseAuth.instance.signOut();
+  }  
+}
+
--- smart_healthy_food/lib/app_screens/login.dart
+++ smart_healthy_food/lib/app_screens/login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:smart_healthy_food/app_screens/auth.dart';
+import 'package:smart_healthy_food/app_screens/signup.dart';
 
 class LoginPage extends StatefulWidget{
   @override
@@ -8,11 +10,277 @@ class LoginPage extends StatefulWidget{
 }
 
 class _LoginPageState extends State<LoginPage>{
+  var _formKey=GlobalKey<FormState>();
+  double _minimumPadding = 5.0;
+
+  bool _isHiddenPw = true;
+  bool _isHiddenCPw = true;
+
+  void _visiblePw() {
+    setState(() {
+      _isHiddenPw = !_isHiddenPw;
+      _isHiddenCPw = _isHiddenCPw;
+    });
+  }
+
+  void _visibleCPw() {
+    setState(() {
+      _isHiddenPw = _isHiddenPw;
+      _isHiddenCPw = !_isHiddenCPw;
+    });
+  }
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('data'),
+      body: logInForm(),
     );
   }
 
+
+  Widget logInForm(){
+    TextStyle textStyle=Theme.of(context).textTheme.title;
+    return Container(
+      alignment: Alignment.center,
+      child: Form(
+        key: _formKey,
+        child: Padding(
+          padding: EdgeInsets.only(
+            right: _minimumPadding*3,
+            left: _minimumPadding * 3,
+            top: _minimumPadding * 3,
+          ),
+          child: ListView(
+            children: <Widget>[
+              SizedBox(
+                height: 260.0,
+              ),
+              //E mail
+              Padding(
+                padding: EdgeInsets.only(bottom: _minimumPadding),
+                child: TextFormField(
+                  controller: emailController,
+                  validator: (String value) {
+                    if (value.isEmpty) {
+                      return "Enter the E mail Address";
+                    }
+                    return null;
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                  style: textStyle,
+                  decoration: InputDecoration(
+                    labelText: "Email",
+                    prefixIcon: Icon(Icons.email),
+                    labelStyle: Theme.of(context).textTheme.body1,
+                    errorStyle: TextStyle(
+                      color: Colors.redAccent,
+                      fontSize: 15.0,
+                    ),
+                    hintText: "Johnperera@gmail.com",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                ),
+              ),
+
+              //Password
+              Padding(
+                padding: EdgeInsets.only(
+                    top: _minimumPadding, bottom: _minimumPadding),
+                child: TextFormField(
+                  controller: passwordController,
+                  validator: (String value) {
+                    if (value.isEmpty) {
+                      return "Enter the Password";
+                    }
+                    return null;
+                  },
+                  keyboardType: TextInputType.text,
+                  style: textStyle,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.lock_outline),
+                    suffixIcon: IconButton(
+                      onPressed: _visiblePw,
+                      icon: _isHiddenPw
+                          ? Icon(Icons.visibility_off)
+                          : Icon(Icons.visibility),
+                    ),
+                    labelText: "Password",
+                    labelStyle: Theme.of(context).textTheme.body1,
+                    errorStyle: TextStyle(
+                      color: Colors.redAccent,
+                      fontSize: 15.0,
+                    ),
+                    hintText: "Password",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                  obscureText: _isHiddenPw,
+                ),
+              ),
+
+              //login
+              Container(
+                height: 70.0,
+                padding: EdgeInsets.only(left: 65.0,right: 65.0,top: 10.0, bottom: 10.0),
+                child: OutlineButton(
+                  splashColor: Colors.green[400],
+                  onPressed: () async{
+                    if(emailController.text.isEmpty||passwordController.text.isEmpty){
+                      print('Enter these');
+                      return;
+                    }else{
+                      String userID=await AuthService().signInWithEmailAndPassword(emailController.text, passwordController.text);
+                      if(userID!=null){
+                        Navigator.pop(context);
+                        Navigator.push(context,
+                          MaterialPageRoute(
+                          )
+                        );
+                      }
+                      //TODO: Set State
+                    }
+                  },
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+                  highlightElevation: 0,
+                  borderSide: BorderSide(color: Colors.green[400]),
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10),
+                          child: Text(
+                            'Log in',
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.black,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+
+              //Text
+              Center(
+                child: Text(
+                  'OR',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 14.0,
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.w600
+                    ),
+                ),
+              ),
+              //sign up with google
+              Container(
+                padding: EdgeInsets.only(top: 10.0),
+                color: Colors.white,
+                child: Center(
+                 child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      _signInButton(),
+                    ],
+                  ),
+                ),
+              ),
+
+              //sign up
+              Center(
+                child: Row(
+                  children: <Widget>[
+                    SizedBox(
+                      width: 80.0,
+                    ),
+                    Text(
+                      "Don't Have Account ? ",
+                      style: TextStyle(
+                        color: Colors.green[400],
+                        fontSize: 16.0,
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.w700
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    GestureDetector(
+                      onTap: (){
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => SignUp()),
+                        );
+                      },
+                      child: Text(
+                        "Sign Up",
+                        style: TextStyle(
+                          color: Colors.green[400],
+                          fontSize: 16.0,
+                          fontStyle: FontStyle.normal,
+                          fontWeight: FontWeight.w700
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+  Widget _signInButton() {
+    return OutlineButton(
+      splashColor: Colors.grey,
+      onPressed: () async{
+        bool res=await AuthService().signInWithGoogle();
+        if(!res){
+          print('Error Log in with google');
+        }else{
+          //TODO : implement to navigator to home page
+        }
+        setState(() {
+         //todo : set state 
+        });
+      },
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+      highlightElevation: 0,
+      borderSide: BorderSide(color: Colors.grey),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image(image: AssetImage("images/google_logo.png"), height: 35.0),
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Text(
+                'Sign in with Google',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.grey,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
+
